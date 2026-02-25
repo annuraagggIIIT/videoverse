644f34f57f3f8b842b05e27ddeb823dc7bc5651c
annuraagggiiit <anuragkhanna@iiitmanipur.ac.in>
2026-02-25 22:46:39 +0530

    added middlewares

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 
 export const db = new Database("database.db");
-
+console.log("Connected to the database");
 db.exec(`
 CREATE TABLE IF NOT EXISTS items (
   id TEXT PRIMARY KEY,
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import "./db/database.js";
+import { notFound } from "./middlewares/notFound.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
@@ -12,6 +15,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("VideoVerse API Ready");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
